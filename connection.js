b6345a80812db3d89c11dc8888a1106bc67af567
ahon54 <55165000+ahon54@users.noreply.github.com>
2020-05-11 17:56:32 -0700

    changes

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const util = require('util');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') })
-
+// require('dotenv').config()
 
 const mysqlConnection = mysql.createConnection({
     host : process.env.DSN,
